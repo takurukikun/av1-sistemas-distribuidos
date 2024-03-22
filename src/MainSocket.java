2e31903509b629aa5a693f611546c19e6766a7b8
Levi Gleik <leviacedo1@gmail.com>
2024-03-22 19:37:41 -0300

    working

--- src/MainSocket.java
+++ src/MainSocket.java
@@ -6,8 +6,6 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class MainSocket {
 
@@ -19,74 +17,72 @@ public class MainSocket {
 
       while (true) {
         Socket clientSocket = serverSocket.accept();
-        InputStream inputStream = clientSocket.getInputStream();
-        OutputStream outputStream = clientSocket.getOutputStream();
-        PrintWriter printWriter = new PrintWriter(outputStream, true);
-
-        int i;
-        char scape = '?';
-
-        printOptions(printWriter);
-        while ((i = inputStream.read()) != -1) {
-          if (i == scape) {
-            printOptions(printWriter);
-            break;
+        new Thread(() -> {
+          try {
+            handleClient(clientSocket);
+          } catch (IOException e) {
+            e.printStackTrace();
           }
-
-          char c = (char) i;
-          String input = "";
-          while (c != '\n') {
-            input += c;
-            c = (char) inputStream.read();
-          }
-
-          input = input.trim();
-          if (input.equals("1")) {
-            printWriter.println("Digite o primeiro número: ");
-            float a = Float.parseFloat((new BufferedReader(new InputStreamReader(inputStream)).readLine()));
-            printWriter.println("Digite o segundo número: ");
-            float b = Float.parseFloat((new BufferedReader(new InputStreamReader(inputStream)).readLine()));
-            printWriter.println("Resultado: " + (a + b));
-            printOptions(printWriter);
-          } else if (input.equals("2")) {
-            printWriter.println("Digite o primeiro número: ");
-            float a = Float.parseFloat((new BufferedReader(new InputStreamReader(inputStream)).readLine()));
-            printWriter.println("Digite o segundo número: ");
-            float b = Float.parseFloat((new BufferedReader(new InputStreamReader(inputStream)).readLine()));
-            printWriter.println("Resultado: " + (a - b));
-            printOptions(printWriter);
-          } else if (input.equals("3")) {
-            printWriter.println("Digite o primeiro número: ");
-            float a = Float.parseFloat((new BufferedReader(new InputStreamReader(inputStream)).readLine()));
-            printWriter.println("Digite o segundo número: ");
-            float b = Float.parseFloat((new BufferedReader(new InputStreamReader(inputStream)).readLine()));
-            printWriter.println("Resultado: " + (a * b));
-            printOptions(printWriter);
-          } else if (input.equals("4")) {
-            printWriter.println("Digite o primeiro número: ");
-            float a = Float.parseFloat((new BufferedReader(new InputStreamReader(inputStream)).readLine()));
-            printWriter.println("Digite o segundo número: ");
-            float b = Float.parseFloat((new BufferedReader(new InputStreamReader(inputStream)).readLine()));
-            printWriter.println("Resultado: " + (a / b));
-            printOptions(printWriter);
-          } else {
-            printWriter.println("Opção inválida");
-            printOptions(printWriter);
-          }
-        }
-        // clientSocket.close();
+        }).start();
       }
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
-  public static void printOptions(PrintWriter printWriter) {
+  public static void printOptions(PrintWriter printWriter, int op) {
+    if (op == 0) {
+      printWriter.println("Bem-vindo ao calculadora server");
+      printWriter.println("-----------------------------");
+      printWriter.println("");
+    }
+
     printWriter.println("Selecione uma das opções: (digite ? a qualquer momento para ver as opções)");
     printWriter.println("1 - Somar");
     printWriter.println("2 - Subtrair");
     printWriter.println("3 - Multiplicar");
     printWriter.println("4 - Dividir");
+
+    if (op == 1) {
+      printWriter.println("Você selecinou '1 - somar'");
+
+    } else if (op == 2) {
+      printWriter.println("Você selecinou '2 - subtrair'");
+
+    } else if (op == 3) {
+      printWriter.println("Você selecinou '3 - multiplicar'");
+
+    } else if (op == 4) {
+      printWriter.println("Você selecinou '4 - dividir'");
+    }
+
+    printWriter.println("-----------------------------");
+    printWriter.println("");
+  }
+
+  public static float calculate(float a, float b, char operator) {
+    switch (operator) {
+      case '+':
+        return a + b;
+      case '-':
+        return a - b;
+      case '*':
+        return a * b;
+      case '/':
+        return a / b;
+    }
+
+    return 0;
+  }
+
+  public static float[] getNumbers(InputStream inputStream, PrintWriter printWriter) throws IOException {
+    float[] numbers = new float[2];
+    printWriter.println("Digite o primeiro número: ");
+
+    numbers[0] = Float.parseFloat((new BufferedReader(new InputStreamReader(inputStream)).readLine()));
+    printWriter.println("Digite o segundo número: ");
+    numbers[1] = Float.parseFloat((new BufferedReader(new InputStreamReader(inputStream)).readLine()));
+    return numbers;
   }
 
   // public static String calculate(String input) {
@@ -114,4 +110,62 @@ public class MainSocket {
   // return "Invalid input";
 
   // }
+  public static void handleClient(Socket clientSocket) throws IOException {
+    InputStream inputStream = clientSocket.getInputStream();
+    OutputStream outputStream = clientSocket.getOutputStream();
+    PrintWriter printWriter = new PrintWriter(outputStream, true);
+
+    int i;
+    char scape = '?';
+
+    printOptions(printWriter, 0);
+    while ((i = inputStream.read()) != -1) {
+
+      if (i == scape) {
+        printOptions(printWriter, 0);
+      }
+
+      char c = (char) i;
+      String input = "";
+      while (c != '\n') {
+        input += c;
+        c = (char) inputStream.read();
+      }
+
+      input = input.trim();
+      try {
+        if (input.equals("1")) {
+          float[] numbers = getNumbers(inputStream, printWriter);
+          printWriter.println("Resultado: " + (numbers[0] + numbers[1]));
+          printOptions(printWriter, 1);
+        } else if (input.equals("2")) {
+          float[] numbers = getNumbers(inputStream, printWriter);
+          printWriter.println("Resultado: " + (numbers[0] - numbers[1]));
+          printOptions(printWriter, 2);
+        } else if (input.equals("3")) {
+          float[] numbers = getNumbers(inputStream, printWriter);
+          printWriter.println("Resultado: " + (numbers[0] * numbers[1]));
+          printOptions(printWriter, 3);
+        } else if (input.equals("4")) {
+          float[] numbers = getNumbers(inputStream, printWriter);
+          float result = numbers[0] / numbers[1];
+          if (Float.isInfinite(result)) {
+            printWriter.println("Divisão por zero");
+            printOptions(printWriter, 4);
+            continue;
+          } else {
+            printWriter.println("Resultado: " + (numbers[0] / numbers[1]));
+          }
+          printOptions(printWriter, 4);
+        } else {
+          printWriter.println("Opção inválida");
+          printOptions(printWriter, 0);
+        }
+      } catch (Exception e) {
+        printWriter.println("Entrada inválida");
+        printOptions(printWriter, 0);
+        continue;
+      }
+    }
+  }
 }
